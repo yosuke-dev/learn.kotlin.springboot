1b5cda1ae179927d24ed8ff8560a4210b088962f
yosuke_dev <devtoku.yosuke@gmail.com>
2021-07-23 23:21:52 +0900

    Add Mybatis

--- src/main/kotlin/lean/kotlin/tutorial/springboot/SpringbootApplication.kt
+++ src/main/kotlin/lean/kotlin/tutorial/springboot/SpringbootApplication.kt
@@ -1,5 +1,8 @@
 package lean.kotlin.tutorial.springboot
 
+import org.apache.ibatis.io.Resources
+import org.apache.ibatis.session.SqlSessionFactory
+import org.apache.ibatis.session.SqlSessionFactoryBuilder
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +11,10 @@ class SpringbootApplication
 
 fun main(args: Array<String>) {
 	runApplication<SpringbootApplication>(*args)
+}
+
+fun createSessionFactory(): SqlSessionFactory {
+	val resource = "mybatis-config.xml"
+	val inputStream = Resources.getResourceAsStream(resource)
+	return SqlSessionFactoryBuilder().build(inputStream)
 }
