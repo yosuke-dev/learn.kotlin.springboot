5605f6c64bde437599d68536874b57994052cc7e
yosuke_dev <devtoku.yosuke@gmail.com>
2021-07-23 23:40:04 +0900

    Update CountSelect

--- src/main/kotlin/lean/kotlin/tutorial/springboot/SelectController.kt
+++ src/main/kotlin/lean/kotlin/tutorial/springboot/SelectController.kt
@@ -3,6 +3,7 @@ package lean.kotlin.tutorial.springboot
 import database.UserDynamicSqlSupport.User.age
 import database.UserMapper
 import database.UserRecord
+import database.count
 import database.select
 import org.mybatis.dynamic.sql.util.kotlin.elements.isGreaterThanOrEqualTo
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,12 +12,30 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class SelectController {
     @GetMapping("/select")
-    fun select() : List<UserRecord> {
+    fun select(): List<UserRecord> {
         createSessionFactory().openSession().use { session ->
             val mapper = session.getMapper(UserMapper::class.java)
             return mapper.select {
                 where(age, isGreaterThanOrEqualTo(25))
             }
+        }
+    }
+
+    @GetMapping("/count")
+    fun count(): Long {
+        createSessionFactory().openSession().use { session ->
+            val mapper = session.getMapper(UserMapper::class.java)
+            return mapper.count {
+                where(age, isGreaterThanOrEqualTo(25))
+            }
+        }
+    }
+
+    @GetMapping("/allCount")
+    fun allCount(): Long {
+        createSessionFactory().openSession().use { session ->
+            val mapper = session.getMapper(UserMapper::class.java)
+            return mapper.count { allRows() }
         }
     }
 }
