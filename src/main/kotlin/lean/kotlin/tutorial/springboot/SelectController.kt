e4fba5b329286a4812cbd7d4aca9e1e94bbc0df3
yosuke_dev <devtoku.yosuke@gmail.com>
2021-07-24 13:38:50 +0900

    Update Insert

--- src/main/kotlin/lean/kotlin/tutorial/springboot/SelectController.kt
+++ src/main/kotlin/lean/kotlin/tutorial/springboot/SelectController.kt
@@ -1,15 +1,12 @@
 package lean.kotlin.tutorial.springboot
 
+import database.*
 import database.UserDynamicSqlSupport.User.age
-import database.UserMapper
-import database.UserRecord
-import database.count
-import database.select
 import org.mybatis.dynamic.sql.util.kotlin.elements.isGreaterThanOrEqualTo
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("user")
 class SelectController {
     @GetMapping("/select")
     fun select(): List<UserRecord> {
@@ -36,6 +33,16 @@ class SelectController {
         createSessionFactory().openSession().use { session ->
             val mapper = session.getMapper(UserMapper::class.java)
             return mapper.count { allRows() }
+        }
+    }
+
+    @PostMapping("/insert")
+    fun insert(@RequestBody request: List<UserRecord>): String {
+        createSessionFactory().openSession().use { session ->
+            val mapper = session.getMapper(UserMapper::class.java)
+            val count = mapper.insertMultiple(request)
+            session.commit()
+            return "$count 行のレコードを挿入しました"
         }
     }
 }
